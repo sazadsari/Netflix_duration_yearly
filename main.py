a9db591b4b2bb01f3db9558344b6a665773d5747
Sol Azadsari <sazadsari414@gmail.com>
2022-05-30 12:04:17 -0500

    test-1

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 
 
 # Create the years and durations lists
-years = [2011,2012,2013,2014,2015,2016,2017,2018,2019,2020]
+years = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
 durations = [103,101,99,100,100,95,95,96,93,90]
 
 # Create a dictionary with the two lists
 movie_dict = {'years':years, 'durations':durations}
 
 # Print the dictionary
-movie_dict
+print(movie_dict)
 
 
 
