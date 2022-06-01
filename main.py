7ebcbe78cb1f8b07113a9edfaa011c7bb61b329d
Sol Azadsari <sazadsari414@gmail.com>
2022-05-31 22:20:18 -0500

    test-1

--- main.py
+++ main.py
@@ -4,18 +4,15 @@ import pandas as pd
 # Import matplotlib.pyplot under its usual alias and create a figure
 import matplotlib.pyplot as plt
 
-
 # Create the years and durations lists
 years = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
-durations = [103,101,99,100,100,95,95,96,93,90]
+durations = [103, 101, 99, 100, 100, 95, 95, 96, 93, 90]
 
 # Create a dictionary with the two lists
-movie_dict = {'years':years, 'durations':durations}
+movie_dict = {'years': years, 'durations': durations}
 
 # Print the dictionary
 print(movie_dict)
-
-
 
 # Create a DataFrame from the dictionary
 durations_df = pd.DataFrame(movie_dict)
@@ -23,11 +20,10 @@ durations_df = pd.DataFrame(movie_dict)
 # Print the DataFrame
 print(durations_df)
 
-
 fig = plt.figure()
 
 # Draw a line plot of release_years and durations
-release_years = plt.plot(years,durations)
+release_years = plt.plot(years, durations)
 
 # Create a title
 release_years = plt.title("Netflix Movie Durations 2011-2020")
@@ -36,7 +32,7 @@ release_years = plt.title("Netflix Movie Durations 2011-2020")
 plt.show()
 
 # Read in the CSV as a DataFrame
-netflix_df = pd.read_csv("datasets/netflix_data.csv")
+netflix_df = pd.read_csv("C:/Users/sol/PycharmProjects/Netflix_duration_yearly/netflix_data.csv")
 
 # Print the first five rows of the DataFrame
 print(netflix_df.head())
@@ -45,16 +41,16 @@ print(netflix_df.head())
 netflix_df_movies_only = netflix_df[netflix_df['type'] == "Movie"]
 
 # Select only the columns of interest
-netflix_movies_col_subset = netflix_df_movies_only[['title','country','genre','release_year','duration']]
+netflix_movies_col_subset = netflix_df_movies_only[['title', 'country', 'genre', 'release_year', 'duration']]
 
 # Print the first five rows of the new DataFrame
 print(netflix_movies_col_subset[0:5])
 
 # Create a figure and increase the figure size
-fig = plt.figure(figsize=(12,8))
+fig = plt.figure(figsize=(12, 8))
 
 # Create a scatter plot of duration versus year
-plt.scatter(netflix_movies_col_subset['release_year'],netflix_movies_col_subset['duration'])
+plt.scatter(netflix_movies_col_subset['release_year'], netflix_movies_col_subset['duration'])
 
 # Create a title
 plt.title("Movie Duration by Year of Release")
@@ -86,10 +82,10 @@ for lab, row in netflix_movies_col_subset.iterrows():
 print(colors[0:10])
 # Set the figure style and initalize a new figure
 plt.style.use('fivethirtyeight')
-fig = plt.figure(figsize=(12,8))
+fig = plt.figure(figsize=(12, 8))
 
 # Create a scatter plot of duration versus release_year
-plt.scatter(netflix_movies_col_subset['release_year'], netflix_movies_col_subset['duration'], c = colors)
+plt.scatter(netflix_movies_col_subset['release_year'], netflix_movies_col_subset['duration'], c=colors)
 
 # Create a title and axis labels
 plt.title("Movie duration by year of release")
@@ -98,5 +94,3 @@ plt.ylabel("Duration (min)")
 
 # Show the plot
 plt.show()
-
-# Did these changes take effect? I wanna know!
